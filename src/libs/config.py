20bc63aeda92be6a0cd02b92591b9a38ae883a97
AJ00200 <aj0020@aj00200.org>
2011-10-15 22:29:22 -0500

    Added libs/config.py - @pharno, if you have a better version uncommited, please feel free to overwrite

---
+++ src/libs/config.py
@@ -0,0 +1,6 @@
+def load_config():
+    '''Load the configuration file from ~/.vomun/config.*
+    TODO: Decide on using a JSON file for a human readable config or if we
+          should use the ConfigParser module.
+    '''
+    pass
