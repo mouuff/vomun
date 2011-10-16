94494e01b5d7cb82fa59692e62c6fb22c759e927
AJ00200 <aj0020@aj00200.org>
2011-10-15 23:16:04 -0500

    More minor changes. Added some errors to libs/errors.py

--- src/libs/config.py
+++ src/libs/config.py
@@ -1,9 +1,16 @@
+'''Manage the configuration file. Load/save/modify it.'''
+import os
+
 def load_config():
     '''Load the configuration file from ~/.vomun/config.*
     TODO: Decide on using a JSON file for a human readable config or if we
           should use the ConfigParser module.
     '''
-    pass
+
+    # Temporary until we load from a file or similar
+    return {
+        'gnupgdir': os.path.expanduser("~/.vomun/friends.json") # unused
+    }
     
 
 load_config()
--- src/libs/errors.py
+++ src/libs/errors.py
@@ -2,4 +2,14 @@ class AnonError(Exception):
     pass
 
 class DependancyError(AnonError):
+    '''We have an unmet dependancy.'''
+    pass
+
+class ConnectionError(AnonError):
+    '''Could not connect to a node.'''
+    pass
+    
+class ProtocolError(AnonError):
+    '''There was an error parsing the protocol. Maybe we are out of date or
+    maybe the node we are connecting to is out of date.'''
     pass
