38ee81c79e240f9484235500be737d140bd18537
AJ00200 <aj0020@aj00200.org>
2011-11-04 17:17:50 -0500

    Minor changes to the crypto code. Next change should put key generation into setup.py

--- src/api/functions.py
+++ src/api/functions.py
@@ -6,7 +6,7 @@ add_queue = []
 
 def register_with_api(func):
     try:
-        server = libs.globals.global_vars["apiserver"]
+        server = libs.globals.global_vars['apiserver']
         server.add_call(func)
     except:
         add_queue.append(func)
@@ -14,13 +14,13 @@ def register_with_api(func):
 
 @register_with_api
 def get_functions():
-    """lists the api functions"""
-    return [func.__name__ for func in libs.globals.global_vars["apiserver"].calls]
+    '''lists the api functions'''
+    return [func.__name__ for func in libs.globals.global_vars['apiserver'].calls]
 
 @register_with_api
 def help(funcname):
     func = None
-    for f in  libs.globals.global_vars["apiserver"].calls:
+    for f in  libs.globals.global_vars['apiserver'].calls:
         if f.__name__ == funcname:
             func = f
             break
@@ -28,14 +28,19 @@ def help(funcname):
      
 @register_with_api
 def shutdown():
-    """shuts down the server"""
+    '''shuts down the server'''
     libs.globals.global_vars['running'] = False
     libs.threadmanager.killall()
     libs.threadmanager.close_sockets()
     return True
 
+@register_with_api
+def get_build():
+    '''Return the build number'''
+    return libs.globals.global_vars['anon+']['BUILD']
+
 def register():
-    server = libs.globals.global_vars["apiserver"]
+    server = libs.globals.global_vars['apiserver']
     for call in add_queue:
         server.add_call(call)
 
