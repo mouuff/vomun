5ef913b3c89c4d19df11b63c74836c63a52f6c0c
AJ00200 <aj0020@aj00200.org>
2011-11-05 08:59:55 -0500

    Updated setup.py to create the proper file path, use the proper seperating character depending on the OS, and generate keys. The fingerprints for these keys are stored in the config.json file which is also generated.

--- src/setup.py
+++ src/setup.py
@@ -6,29 +6,111 @@ print('''
 import os
 import libs.errors
 
-# Import gnupg
 try:
     import gnupg
 except ImportError:
     raise libs.errors.DependancyError(
                 'Please install https://code.google.com/p/python-gnupg/')
-                                      
 
-# Create ~/.vomun/
-print('[*] Making ~/.vomun/')
+## Prepare for setup
+# Find local variables
+print('[*] Preparing for setup...')
+HOME = os.getenv('HOME')
+VOMUN_PATH = os.path.join(HOME, '.vomun')
+KEYS_PATH = os.path.join(VOMUN_PATH, 'keys')
+CONFIG_PATH = os.path.join(VOMUN_PATH, 'config.json')
+
+USER_NAME = raw_input('Pick a user name: ')
+
+# Setup file structure for Anon+
+try:
+    print(' [*] Making ~/.vomun/')
+    os.mkdir(VOMUN_PATH, 0711)
+except OSError as error:
+    if error.errno != 17:
+        print('  [*] Error creating %s' % VOMUN_PATH)
+        raise libs.errors.InstallError(
+                     'Please check your file permissions for %s' % HOME)
+    else:
+        print('  [*] %s already exists. Do not need to create.' % VOMUN_PATH)
+
+try:
+    print(' [*] Making ~/.vomun/gpg/')
+    os.mkdir(KEYS_PATH, 0700)
+except OSError as error:
+    if error.errno != 17:
+        print('  [*] Error creating %s' % KEYS_PATH)
+        raise libs.errors.InstallError(
+                '      Please check your file permissions on %s' % VOMUN_PATH)
+    else:
+        print('  [*] %s already exists. Do not need to create.' % KEYS_PATH)
+        
+# Setup python-gpg to use ~/.vomun/keys
+try:
+    gpg = gnupg.GPG(gnupghome = KEYS_PATH)
+except ValueError:
+    raise libs.errors.DependancyError('Please install GPG: http://gnupg.org/')
 
 ## Key setup
-print('[*] Setting up encryption keys')
-# Create ~/.vomun/keys/
-print(' [*] Making ~/.vomun/keys/')
-
-# Create ~/.vomun/keys/private/
-print(' [*] Making ~/.vomun/keys/private/')
-
 # Generate 2048 bit node key
+print('[*] Setting up encryption keys')
 print(' [*] Generating a 2048 bit node key')
 print('     this could take a while...')
+
+key_data = gpg.gen_key_input(key_type = 'RSA', key_length = 2048,
+                             name_real = 'Anonymous Node Key')
+key = gpg.gen_key(key_data)
+fingerprint = key.fingerprint
+
+print('  [*] Done. Key fingerprint:')
+print('      ' + fingerprint)
 
 # Generate 2048 bit identity key
 print(' [*] Generating a 2048 bit identity key')
 print('     this could take a while...')
+
+idkey_data = gpg.gen_key_input(key_type = 'RSA', key_length = 2048,                               
+                               name_real = USER_NAME) # TODO: Allow password
+idkey = gpg.gen_key(idkey_data)
+idfingerprint = idkey.fingerprint
+
+print('  [*] Done. Key fingerprint:')
+print('      ' + idfingerprint)
+
+## Configuration
+# Generate the contents
+print('[*] Generating the config file...')
+template = '''                                                                                                                                                       
+{
+    "gnupgdir": "{keysdir}",
+    "vomundir": "{vomundir}",
+    "nodekey": "{nodekey}",
+    "userkey": "{userkey}",
+    "username": "{username}"
+}
+'''
+config = template.format(keysdir = KEYS_PATH,
+                         vomundir = VOMUN_PATH,
+                         nodekey = fingerprint,
+                         userkey = idfingerprint,
+                         username = USER_name
+                        )
+
+try:
+    print(' [*] Writing the config file.')
+    config_file = open(CONFIG_PATH, 'w')
+    config_file.write(config)
+    config_file.close()
+except IOError as error:
+    print('  [*] Error writing %s. Check file permissions' % CONFIG_PATH)
+    raise libs.errors.InstallError('Could not write to %s.' % CONFIG_PATH)
+
+
+## Setup complete
+print(' == Setup Complete ==')
+print('''
+ == Anon+ Setup is Complete ==
+Please run vomun.py and then go to
+http://localhost:++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++7777/ to use it.
+''')
+
